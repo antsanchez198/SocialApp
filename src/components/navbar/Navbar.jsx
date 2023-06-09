b17cce844f783c0a0a4d9d4ec55fb99db1b931f8
Anthony Sanchez <antsanchez198@gmail.com>
2023-06-09 01:53:53 -0400

    adding database:

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link href="/feed">Feed</Link>
+      <Link href="/search">Search</Link>
+      <Link href="/post">Post</Link>
+      <Link href="/profile">Profile</Link>
+      <button>Log Out</button>
+    </div>
   )
 }
 
